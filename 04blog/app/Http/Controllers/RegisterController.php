1210e0416c6700c96aafd88feaaaf740f6528b76
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2023-02-16 21:26:43 -0500

    registro

--- 04blog/app/Http/Controllers/RegisterController.php
+++ 04blog/app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -14,7 +17,19 @@ class RegisterController extends Controller
         // dd('formulario enviado');
         // dd($request);
         $this->validate($request, [
-            'name' => ['required', 'min:5']
+            'name' => ['required', 'min:5'],
+            'username' => "required|unique:users|min:3|max:20",
+            'email' => ['required', 'unique:users','email', 'max:50'],
+            'password' => ['required', 'confirmed', 'min:6']
+        ]);
+
+        // dd('generando usuario');
+        User::create([
+            'name' => $request->name,
+            // 'username' => Str::lower($request->username),
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
         ]);
     }
 }
--- 04blog/resources/views/auth/register.blade.php
+++ 04blog/resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
             @csrf
             <div class="form-group mb-3">
                 <label for="name" class="mb-1">Nombre</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="ingresa tu Nombre">
+                <input type="text" class="form-control @error('name') border-danger @enderror" id="name" name="name" placeholder="ingresa tu Nombre" value="{{ old('name') }}">
                 @error('name')
                     <div class="text-danger">
                         {{ $message }}
@@ -23,15 +23,30 @@
             </div>
             <div class="form-group mb-3">
                 <label for="username" class="mb-1">Nombre de usuario</label>
-                <input type="text" class="form-control" id="username" name="username" placeholder="ingresa tu Nombre de usuario">
+                <input type="text" class="form-control @error('username') border-danger @enderror" id="username" name="username" placeholder="ingresa tu Nombre de usuario" value="{{ old('username') }}">
+                @error('username')
+                    <div class="text-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="form-group mb-3">
                 <label for="email" class="mb-1">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="ingresa tu email">
+                <input type="text" class="form-control @error('email') border-danger @enderror" id="email" name="email" placeholder="ingresa tu email" value="{{ old('email') }}">
+                @error('email')
+                    <div class="text-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="form-group mb-3">
                 <label for="password" class="mb-1">Password</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Ingresa tu password">
+                <input type="password" class="form-control @error('password') border-danger @enderror" id="password" name="password" placeholder="Ingresa tu password">
+                @error('password')
+                    <div class="text-danger">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="form-group mb-3">
                 <label for="password_confirmation" class="mb-1">Password</label>
